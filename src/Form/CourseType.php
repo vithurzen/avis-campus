0bf2f20d674c47af4582b303281e965ce6249d38
vithurzen <vithurzen517@gmail.com>
2026-06-30 13:29:39 +0200

    crud controller

---
+++ src/Form/CourseType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use App\Entity\Semester;
+use App\Entity\Tag;
+use App\Entity\Teacher;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('coefficient')
+            ->add('hours')
+            ->add('createdAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('semester', EntityType::class, [
+                'class' => Semester::class,
+                'choice_label' => 'id',
+            ])
+            ->add('teachers', EntityType::class, [
+                'class' => Teacher::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
---
+++ src/Form/SemesterType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use App\Entity\Semester;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SemesterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('number')
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Semester::class,
+        ]);
+    }
+}
