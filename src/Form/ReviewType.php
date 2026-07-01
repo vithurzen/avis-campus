03ecb8069b3cf62e2ac605dc05fef864aba0282c
Jelain John Jesuratnam <jelainjohn@outlook.fr>
2026-07-01 15:01:06 +0200

    feat: reviews

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,21 +4,34 @@ namespace App\Form;
 
 use App\Entity\Course;
 use App\Entity\Review;
+use App\Repository\CourseRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReviewType extends AbstractType
 {
+    public function __construct(
+        private readonly Security $security,
+        private readonly CourseRepository $courseRepository,
+    ) {}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $currentUser = $this->security->getUser();
+        $currentCourse = $builder->getData()?->getCourse();
+
         $builder
             ->add('title')
             ->add('content')
             ->add('course', EntityType::class, [
                 'class' => Course::class,
                 'choice_label' => 'title',
+                'query_builder' => $currentUser
+                    ? $this->courseRepository->findCoursesNotReviewedBy($currentUser, $currentCourse)
+                    : null,
             ])
         ;
     }
